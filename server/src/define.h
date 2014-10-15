b07423e7c0b18972e85f437d1307d9b6f68b93bb
= <migrant.tao@gmail.com>
2014-10-15 18:30:52 +0800

    add server src

---
+++ server/src/define.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	 File Name: define.h
+	 Author: Jacky.Tao 
+	 Mail: migrant.tao@gmail.com 
+	 Created Time: 2014年10月15日 星期三 17时18分07秒
+ ************************************************************************/
+
+#ifndef _DEFINE_H
+#define _DEFINE_H
+
+#define SERVERIP = "192.168.10.89"
+
+#endif
